2b08a7561e796b101d8bca56c837dfbbe7bd94bd
Eshaan Bansal <eshaan7bansal@gmail.com>
2020-01-25 22:05:09 +0530

    bugs

--- Frontend/src/app/@theme/components/footer/footer.component.ts
+++ Frontend/src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b>Team ByteKarma</b>
+    <span class="created-by">Created with ♥ by <b><a target="_blank" href="https://eshaan7.github.io/">Eshaan7</a></b>
   `,
 })
 export class FooterComponent {
